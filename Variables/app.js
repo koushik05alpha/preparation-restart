8968947a1d74d05b8dfca133e6596ba83c4a707d
koushik05alpha <koushiksaha0005@gmail.com>
2025-07-15 01:05:13 +0600

    location object learnnig

--- Variables/app.js
+++ Variables/app.js
@@ -1,7 +1,12 @@
+// js a 4 vaba cholk naya jay
+
 a = 10
 
+// atar man chnage kora jaba
 var b = 20
 
+// atar man chnage kora jaba 
 let c = 60
 
-const d = 20
+// atar man chnage kora jaba na
+const d = 20 
